5e1a5ad13fe25dbe9f9f9482896b93a3b37c3f2d
KOFIT <erentaskin.34@icloud.com>
2020-07-20 13:40:22 +0300

    Break-Continue

---
+++ Break-Continue/Break-Continue/Break-Continue.cpp
@@ -0,0 +1,65 @@
+// Break-Continue.cpp //
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    for (int i = 0; i < 10; i++) {
+        cout << "This is a example of break " << i << "\n";
+
+        if (i == 5) {
+            break;
+        }
+
+        cout << "If this works you will not see here after 5. \n";
+        cout << "******************************************** \n";
+    }
+    cout << "If you see this after 5 this code works.";
+    */
+    
+
+    /*
+    for (int i = 0; i < 10; i++) {
+        cout << "This is a example of continue " << i << "\n";
+
+        if (i == 5) {
+            continue;
+        }
+
+        cout << "If you see here in 5 review your code." "\n";
+        cout << "**************************************" "\n";
+    }
+    */
+
+
+    /*
+    //Declaring Variables
+    const string password = "DoWhile";
+    string input;
+
+    //Making do while loop
+    do {
+
+        cout << "Enter your password > ";
+        cin >> input;
+
+        if (input == password)
+        {
+            break;
+        }
+        else 
+        {
+            cout << "Acces denied.\n";
+            cout << "*************\n" ;
+        }
+
+    } while (1);
+
+    //If entered password in do while loop is true this will happen
+    cout << "Acces granted.\n";
+    cout << "**************\n";
+    */
+}
+
