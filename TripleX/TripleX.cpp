44902bc55b6f80723a8ba454011d0b85c46c557a
KOFIT <erentaskin.34@icloud.com>
2020-10-02 14:34:53 +0300

    .

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{ 
+void PrintIntroduction ()
+{
     // Printing story
-    cout << "You are a prisoner in the jail.\n";
+    cout << "\nYou are a prisoner in the jail.\n";
     cout << "You need escape from here.\n";
     cout << "But to escape from here you need to enter the right numbers to jail door keypads.\n";
     cout << "Good luck.\n";
     cout << "*********************************************************************************\n";
+}
 
+bool PlayGame ()
+{
     // Declaring variables
     int CodeA = 2;
     int CodeB = 3;
@@ -19,28 +22,41 @@ int main()
     int CodeProduct = CodeA * CodeB * CodeC;
 
     // Printing variables
-    cout << "There are 3 numbers of code.\n";
+    cout << "\nThere are 3 numbers of code.\n";
     cout << "The codes add-up to: " << CodeSum;
     cout << "\n";
     cout << "The codes multiply to give: " << CodeProduct << "\n";
 
+    // Storing player guesses
     int GuessA, GuessB, GuessC;
     cout << "What is your guess? ";
-    cin >> GuessA;
-    cin >> GuessB;
-    cin >> GuessC;
+    cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Checking is player guess is correct
     if (CodeSum == GuessSum && CodeProduct == GuessProduct)
     {
-        cout << "You escaped. Well done.";
+        cout << "\nYou escaped. Well done.\n";
+        return true;
     }
     else
     {
-        cout << "You couldn't escaped.";
+        cout << "\nYou couldn't escaped.\n";
+        return false;
+    }
+}
+
+int main() 
+{ 
+    PrintIntroduction();
+    while(true)
+    {
+        bool bLevelComplete = PlayGame();
+        cin.clear(); // Clears any error
+        cin.ignore(); // Discards the buffer
     }
 
-    return 0; 
+    return 0;                                   
 }
